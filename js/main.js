9870359d3d5f0263bc1c0b7a8c446d5cc2343b9f
Neikan <sladkov.e.m@outlook.com>
2020-02-26 18:59:59 +0300

    Завершение. Часть 1

--- js/main.js
+++ js/main.js
@@ -2,20 +2,78 @@
 
 (function () {
 
-  var CLASS_FOR_DISPLAY_FORM = 'visually-hidden';
+  var ClassForManipulation = {
+    HIDDEN: 'visually-hidden',
+    SEARCH_FORM_OPEN: 'search-form-container-open',
+    MESSAGE_ERROR: 'message-error',
+    MESSAGE_ERROR_TEXT: 'message-error-text',
+    MINUS: 'operation-minus',
+    PLUS: 'operation-plus'
+  };
+
+  var KeyCode = {
+    ENTER: 13,
+    ESC: 27
+  };
 
   var searchFormDisplayButton = document.querySelector('.search-form-display');
   var searchForm = document.querySelector('.search-form-container');
-  searchForm.classList.add(CLASS_FOR_DISPLAY_FORM);
+  var inputDateArrival = searchForm.querySelector('#date-arrival');
+  var inputDateDeparture = searchForm.querySelector('#date-departure');
+  var inputAdults = searchForm.querySelector('#adults');
+  var minusAdults = searchForm.querySelector('#adults-minus');
+  var plusAdults = searchForm.querySelector('#adults-plus');
+  var inputChildren = searchForm.querySelector('#children');
+  var minusChildren = searchForm.querySelector('#children-minus');
+  var plusChildren = searchForm.querySelector('#children-plus');
+
+  var openSearchForm = function () {
+    searchForm.classList.remove(ClassForManipulation.HIDDEN);
+    searchForm.classList.add(ClassForManipulation.SEARCH_FORM_OPEN);
+  };
+  var closeSearchForm = function () {
+    searchForm.classList.add(ClassForManipulation.HIDDEN);
+    searchForm.classList.remove(ClassForManipulation.SEARCH_FORM_OPEN);
+  };
 
   var searchFormDisplayButtonClickHandler = function () {
-    if (searchForm.getAttribute('class') === 'search-form-container ' + CLASS_FOR_DISPLAY_FORM) {
-      searchForm.classList.remove(CLASS_FOR_DISPLAY_FORM);
+    if (searchForm.classList.contains(ClassForManipulation.HIDDEN)) {
+      openSearchForm();
     } else {
-      searchForm.classList.add(CLASS_FOR_DISPLAY_FORM);
+      closeSearchForm();
+    }
+  };
+
+  var documentKeyDownHandler = function (evt) {
+    if (evt.keyCode === KeyCode.ESC) {
+      closeSearchForm();
     }
   };
 
   searchFormDisplayButton.addEventListener('click', searchFormDisplayButtonClickHandler);
+  document.addEventListener('keydown', documentKeyDownHandler);
 
+  minusAdults.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains(ClassForManipulation.MINUS) && inputAdults.value > 1) {
+      --inputAdults.value;
+    }
+  });
+
+  plusAdults.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains(ClassForManipulation.PLUS)) {
+      ++inputAdults.value;
+    }
+  });
+
+  minusChildren.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains(ClassForManipulation.MINUS) && inputChildren.value > 0) {
+      --inputChildren.value;
+    }
+  });
+
+  plusChildren.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains(ClassForManipulation.PLUS)) {
+      ++inputChildren.value;
+    }
+  });
 })();
